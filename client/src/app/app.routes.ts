752e7ce74c919309eee0cb5ff0672b1910e0c880
jimbucc <James.Bucciferro@PERKINELMER.COM>
2025-01-02 05:59:14 -0500

    End of Section 7

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -17,9 +20,10 @@ export const routes: Routes = [
             {path: 'members/:id', component: MemberDetailComponent},
             {path: 'lists', component: ListsComponent},
             {path: 'messages', component: MessagesComponent},
-
         ]
     },
-    
+    {path: 'not-found', component: NotFoundComponent},
+    {path: 'errors', component: TestErrorsComponent},
+    {path: 'server-error', component: ServerErrorComponent},    
     {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
